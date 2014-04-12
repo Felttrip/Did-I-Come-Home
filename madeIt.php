acc0a3637418cab0ca72e6f60d0493dc1a74e15f
Nate Thompson <nthompson55@gmail.com>
2014-04-12 12:46:10 -0500

    changed madeIt.php

--- madeIt.php
+++ madeIt.php
@@ -4,19 +4,10 @@ if (isset( $_POST['submit']))
 {
 	pg_prepare($conn,"remove","DELETE FROM dich.trip WHERE user_id=$1");
 	pg_execute($conn,"remove",array($_POST["userid"]));
+	echo "<!DOCTYPE html><html><head><title>You Made It!</title></head><body><h1>Glad you made it home safe!</h1><div><a href="/DidIComeHome/index.php">Want to plan another trip?</a></div></body></html>";
 }
 else
 {
 	header("Location: /DidIComeHome/index.php");
 }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>You Made It!</title>
-</head>
-<body>
-<h1>Glad you made it home safe!</h1>
-<div><a href="/DidIComeHome/index.php">Want to plan another trip?</a></div>
-</body>
-</html>
