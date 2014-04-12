9d62a37c7ec1520f04064cf67cdfe1d311cecf6b
Nate Thompson <nthompson55@gmail.com>
2014-04-12 15:06:11 -0500

    Added boot strap
    because its a hackathon

--- index.php
+++ index.php
@@ -1,17 +1,30 @@
-<!doctype>
-<html>
-<head>
-<title>
-	Did I Come Home?
-</title>
-<link rel="stylesheet" type="text/css" href="style.css">
-</head>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Did you make it home?</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
 <body>
-<h1>Did I Come Home</h1>
+<div class="container">
+<h1>Did you make it home?</h1>
 <?php
 if (isset( $_POST['submit']))
 {
-	include('../CS3380/secure/database.php');
+	//include('../CS3380/secure/database.php');
+    include('../secure/database.php');
+
     $conn = pg_connect(HOST." ".DBNAME." ".USERNAME." ".PASSWORD) or die('Could not connect:' . pg_last_error());
 	$name = htmlspecialchars($_POST["name"]);
 	$location = htmlspecialchars($_POST["location"]);
@@ -38,9 +51,10 @@ if (isset( $_POST['submit']))
 	<div>When should you be home?<br><input type="date" name="endDate" required/></div>
 	<div>What is your email address?<br><input type="email" name="userEmail" autocomplete="off" required/></div>
 	<div>Who should we contact if you don't come back in time?<br><input type="email" name="contactEmail" autocomplete="off" required:/></div>
-	<div><textarea rows="4" cols="50" name = "message">Anything else you want to tell your contact person?</textarea>
+	<div><textarea rows="4" cols="50" name = "message">Anything else you want to tell your contact person?</textarea><br>
     <input type="submit" name="submit" value="Submit" />
 </form>
+</div>
 </div>
 </body>
 </html>
