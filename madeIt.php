c0a4a1fb924c57f235cf7d0b46d54b16927c8f4b
Nate Thompson <nthompson55@gmail.com>
2014-04-12 12:34:36 -0500

    added the post redirect page

--- madeIt.php
+++ madeIt.php
@@ -2,10 +2,21 @@
 <?php
 if (isset( $_POST['submit']))
 {
-	echo "<html><body>".$_POST["userid"]."</body></html>";
+	pg_prepare($conn,"remove","DELETE FROM dich.trip WHERE user_id=$1");
+	pg_execute($conn,"remove",array($_POST["userid"]));
 }
 else
 {
 	header("Location: /DidIComeHome/index.php");
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>You Made It!</title>
+</head>
+<body>
+<h1>Glad you made it home safe!</h1>
+<div><a href="/DidIComeHome/index.php">Want to plan another trip?</a></div>
+</body>
+</html>
